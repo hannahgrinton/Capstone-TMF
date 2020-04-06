330bf2337f74095f1706d34f48c83250bb846c0f
Hannah Grinton <hannahgrinton@gmail.com>
2020-04-06 11:41:10 -0300

    added dbase file that works on remote db

--- TMFadmin/Models/Connection.cs
+++ TMFadmin/Models/Connection.cs
@@ -3,6 +3,8 @@ namespace TMFadmin.Models {
     public class Connection {
         //public const string CONNECTION_STRING = "Server=localhost;Database=tmf-rev;Uid=fuzzbox;Pwd=threejams;SslMode=none;";    
         public const string CONNECTION_STRING = "Server=localhost;Database=dbTMF;Uid=root;Pwd=;SslMode=none;";    
+        //connection string for live hosting:
+        //public const string CONNECTION_STRING = "Server=10.16.112.118;Database=db1120622_TMFfund;Uid=u1120622_TMFfund;Pwd=nsccDonorProject@2020;SslMode=none;";
     }
 
 }
